9294ae452fe0b91947b9acb6ac53c9d26bf45ade
David E. Peña <pexd94.play@gmail.com>
2022-04-26 13:01:00 -0500

    rename folder controllers

--- models/index.js
+++ models/index.js
@@ -1,4 +1,1 @@
-module.exports.Bot = require('./Bot')
-module.exports.Outgoing_Message = require('./Outgoing_Message')
-module.exports.Db_maria = require('./Db_maria')
-module.exports.Db_mysql = require('./Db_mysql')
+module.exports.Outgoing_Message = require('../models/Outgoing_Message')
